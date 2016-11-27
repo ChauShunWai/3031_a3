bdc8c9b2f6077c784115f28328918f8972ce5beb
Sharat V. Madhusudanan <sharatvm@hotmail.co.in>
2016-11-27 20:09:12 +0800

    Started Problem2

--- ass3.pl
+++ ass3.pl
@@ -8,12 +8,29 @@ dot(L1,L2,X) :- length(L1,Len1),
 				\+ (Len1 =:= 0),
 				calcDot(L1,L2,0,X).
 
-calcDot(L1,L2,X1,X) :-  [H1|T1] = L1,
+calcDot(L1,L2,TempX1,X) :-  [H1|T1] = L1,
 						[H2|T2] = L2,
-						X2 is X1 + (H1*H2),
-						calcDot(T1,T2,X2,X).
+						TempX2 is TempX1 + (H1*H2),
+						calcDot(T1,T2,TempX2,X).
 
-calcDot([],[],X1,X1).
+calcDot([],[],TempX1,TempX1).
 
 #Problem[2]
+
+enum (OriginalList, SizeOfSmallerLists, ListOfSmallerLists) :-
+		
+		length(OriginalList, listlen),
+		SizeOfSmallerLists =< listlen,
+		findSmallerLists(OriginalList, SizeOfSmallerLists, TempList, ListOfSmallerLists, LengthOfPrevTail).
+
+findSmallerLists(OriginalList, SizeOfSmallerLists, TempList, ListOfSmallerLists, LengthOfPrevTail) :-
+			
+			[H|T] = OriginalList,
+			
+
+
+
+addNumToSmallList(IncompleteList, OriginalList, TempNewList, NewList) :-
+
+
 
