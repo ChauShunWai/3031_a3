1dfbe70469d1d5d936b2681a1d17196a1d3d1bd6
Sharat V. Madhusudanan <sharatvm@hotmail.co.in>
2016-11-27 19:48:37 +0800

    Added barriers b/w Problems

--- ass3.pl
+++ ass3.pl
@@ -1,3 +1,5 @@
+# Problem [1]
+
 dot([],[],X) :- false.
 
 dot(L1,L2,X) :- length(L1,Len1), 
@@ -12,3 +14,6 @@ calcDot(L1,L2,X1,X) :-  [H1|T1] = L1,
 						calcDot(T1,T2,X2,X).
 
 calcDot([],[],X1,X1).
+
+#Problem[2]
+
