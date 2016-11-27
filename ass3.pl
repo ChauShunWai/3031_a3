d800ed2d73fa4a6357f3caba57c107cb805fc02a
Sharat V. Madhusudanan <sharatvm@hotmail.co.in>
2016-11-27 21:14:01 +0800

    Updated Problem2, no syntax errors now, but logically incorrect

--- ass3.pl
+++ ass3.pl
@@ -1,4 +1,4 @@
-# Problem [1]
+% Problem [1]
 
 dot([],[],X) :- false.
 
@@ -15,22 +15,36 @@ calcDot(L1,L2,TempX1,X) :-  [H1|T1] = L1,
 
 calcDot([],[],TempX1,TempX1).
 
-#Problem[2]
+% Problem [2]
 
-enum (OriginalList, SizeOfSmallerLists, ListOfSmallerLists) :-
+% OneLoop that shifts head of new smaller list by one
+% SecondLoop that adds {size} elements to the new smaller list
+
+enum(OriginalList, SizeOfSmallerLists, ListOfSmallerLists) :-
 		
-		length(OriginalList, listlen),
-		SizeOfSmallerLists =< listlen,
-		findSmallerLists(OriginalList, SizeOfSmallerLists, TempList, ListOfSmallerLists, LengthOfPrevTail).
+		length(OriginalList, Listlen),
+		SizeOfSmallerLists =< Listlen,
+		findSmallerLists(OriginalList, SizeOfSmallerLists, [], ListOfSmallerLists, Listlen).
+
+
+findSmallerLists(OriginalList, SizeOfSmallerLists, TempList, TempList, SizeOfSmallerLists).
+
 
 findSmallerLists(OriginalList, SizeOfSmallerLists, TempList, ListOfSmallerLists, LengthOfPrevTail) :-
 			
-			[H|T] = OriginalList,
-			
+			[H1|T1] = OriginalList,
+			addNumToSmallList([], OriginalList, NewList, SizeOfSmallerLists, 0),
+			append(TempList, [NewList], TempList2),
+			length(T1,LengthOfTail),
+			findSmallerLists(T1, SizeOfSmallerLists, TempList2, ListOfSmallerLists, LengthOfTail).
 
 
-
-addNumToSmallList(IncompleteList, OriginalList, TempNewList, NewList) :-
-
+addNumToSmallList(IncompleteList, [H2|T2], IncompleteList, SizeOfSmallerLists, SizeOfSmallerLists).
 
 
+addNumToSmallList(IncompleteList, [H3|T3], NewList, SizeOfSmallerLists, IterationNum) :-
+
+			IterationNum < SizeOfSmallerLists,
+			append(IncompleteList, [H3], TempNewList),
+			NewIterationNum is IterationNum + 1,
+			addNumToSmallList(TempNewList, T3, NewList, SizeOfSmallerLists, NewIterationNum).
