0559a14322eae4842e6c4797da596c7649c7c160
Sharat V. Madhusudanan <svm@ust.hk>
2016-11-27 17:06:39 +0800

    Modified Sol. 1

--- ass3.pl
+++ ass3.pl
@@ -1,8 +1,12 @@
-dot([],[],X) = false.
+dot([],[],X) :- false.
 
-dot(L1,L2,X) :- length(L1) =:= length(L2),
+dot(L1,L2,X) :- length(L1,Len1), length(L2,Len2),
+				Len1 =:= Len2,
 				[H1|T1] = L1,
 				[H2|T2] = L2,
-				X is X + (L1*L2),
+				checkinit(X),
+				X is X + (H1*H2),
 				dot(T1,T2,X).
 
+checkinit(X) :- \+ must_be(number,X),
+				X=0.
